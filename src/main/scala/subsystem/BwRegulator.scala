a459f0146ce162819bb2db3f571c159d55210eb9
Farzad Farshchi <farzadfr@gmail.com>
2019-06-11 17:17:22 -0500

    do not throttle channel C. writes and read can have programmable cost.

--- src/main/scala/subsystem/BwRegulator.scala
+++ src/main/scala/subsystem/BwRegulator.scala
@@ -6,8 +6,6 @@ import freechips.rocketchip.config._
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.tilelink._
 import freechips.rocketchip.regmapper._
-import freechips.rocketchip.util._
-import TLMessages._
 
 class BwRegulator(
     address: BigInt,
@@ -31,16 +29,17 @@ class BwRegulator(
     val memBase = p(ExtMem).get.base.U
 
     val w = 32
-    val nDomain = n
+    val nDomains = n
     var masterNames = new Array[String](n)
     val enableBW = RegInit(false.B)
+    val wbCost = Reg(UInt(2.W))
+    val rdCost = Reg(UInt(2.W))
     val windowCntr = Reg(UInt(w.W))
     val windowSize = Reg(UInt(w.W))
-    val xactionCntrs = Reg(Vec(nDomain, UInt(w.W)))
-    val maxXactionRegs = Reg(Vec(nDomain, UInt(w.W)))
-    val throttleAcquireGet = RegInit(VecInit(Seq.fill(n)(false.B)))
-    val throttleReleasePut = RegInit(VecInit(Seq.fill(n)(false.B)))
-    val domainId = Reg(Vec(n, UInt(log2Ceil(nDomain).W)))
+    val xactionCntrs = Reg(Vec(nDomains, UInt(w.W)))
+    val maxXactionRegs = Reg(Vec(nDomains, UInt(w.W)))
+    val enableMasters = Reg(Vec(n, Bool()))
+    val domainId = Reg(Vec(n, UInt(log2Ceil(nDomains).W)))
 
     when (windowCntr >= windowSize || !enableBW) {
       windowCntr := 0.U
@@ -53,34 +52,35 @@ class BwRegulator(
       val (out, _) = node.out(i)
       val (in, edge_in) = node.in(i)
 
-      val putOpcodes = Seq(PutFullData, PutPartialData)
       val aIsMemAddr = in.a.bits.address >= memBase
-      val aIsPut = in.a.bits.opcode.isOneOf(putOpcodes)
-      val aCond = aIsMemAddr && (throttleAcquireGet(i) && !aIsPut || throttleReleasePut(i) && aIsPut)
-      val cCond = throttleReleasePut(i) && in.c.bits.opcode === ReleaseData
+      val aIsAcquire = in.a.bits.opcode === TLMessages.AcquireBlock
+      val aCond = aIsMemAddr && aIsAcquire
+      import TLPermissions._
+      val cost = MuxLookup(in.a.bits.param, rdCost,
+        Array(NtoB -> rdCost, NtoT -> (rdCost + wbCost), BtoT -> (rdCost + wbCost)))
 
       out <> in
-      when (enableBW) {
-        when (xactionCntrs(domainId(i)) >= maxXactionRegs(domainId(i))) {
+      when (enableBW && enableMasters(i)) {
+        when (xactionCntrs(domainId(i)) + cost > maxXactionRegs(domainId(i))) {
           when (aCond) {
             out.a.valid := false.B
             in.a.ready := false.B
           }
-          when (cCond) {
-            out.c.valid := false.B
-            in.c.ready := false.B
-          }
         }
-        when (out.a.fire() && aCond || out.c.fire() && cCond) {
-          xactionCntrs(domainId(i)) := xactionCntrs(domainId(i)) + 1.U
+        when (out.a.fire() && aCond) {
+          xactionCntrs(domainId(i)) := xactionCntrs(domainId(i)) + cost
         }
       }
 
       masterNames(i) = edge_in.client.clients(0).name
     }
 
-    val enableBWField = Seq(0 -> Seq(RegField(enableBW.getWidth, enableBW,
-      RegFieldDesc("enableBW", "Enable BW-regulator"))))
+    val enableBWCostField = Seq(0 -> Seq(RegField(enableBW.getWidth, enableBW,
+      RegFieldDesc("enableBW", "Enable BW-regulator")),
+      RegField(rdCost.getWidth, rdCost,
+        RegFieldDesc("rdCost", "Read cost")),
+      RegField(wbCost.getWidth, wbCost,
+        RegFieldDesc("wbCost", "Writeback cost"))))
 
     val windowRegField = Seq(4*1 -> Seq(RegField(windowSize.getWidth, windowSize,
       RegFieldDesc("windowsize", "Size of the window"))))
@@ -89,18 +89,14 @@ class BwRegulator(
       4*(2 + i) -> Seq(RegField(reg.getWidth, reg,
       RegFieldDesc(s"maxxaction$i", s"Maximum number of transactions for domain $i"))) }
 
-    val throttleAcquireGetField = Seq(4*(2+n) -> throttleAcquireGet.zipWithIndex.map { case (bit, i) =>
-      RegField(bit.getWidth, bit, RegFieldDesc("throttleAcquireGet", s"Throttle Acquire and Get for ${masterNames(i)}")) })
-
-    val throttleReleasePutField = Seq(4*(3+n) -> throttleReleasePut.zipWithIndex.map { case (bit, i) =>
-      RegField(bit.getWidth, bit, RegFieldDesc("throttleReleasePut", s"Throttle Release and Put for ${masterNames(i)}")) })
+    val enableMastersField = Seq(4*(2+nDomains) -> enableMasters.zipWithIndex.map { case (bit, i) =>
+      RegField(bit.getWidth, bit, RegFieldDesc("enableMasters", s"Enable BW-regulator for ${masterNames(i)}")) })
 
     val domainIdFields = domainId.zipWithIndex.map { case (reg, i) =>
-      4*(4+n + i) -> Seq(RegField(reg.getWidth, reg,
+      4*(3+nDomains + i) -> Seq(RegField(reg.getWidth, reg,
       RegFieldDesc(s"domainId$i", s"Domain ID for ${masterNames(i)}"))) }
 
-    regnode.regmap(enableBWField ++ windowRegField ++ maxRegFields ++ throttleAcquireGetField ++ throttleReleasePutField
-      ++ domainIdFields: _*)
+    regnode.regmap(enableBWCostField ++ windowRegField ++ maxRegFields ++ enableMastersField ++ domainIdFields: _*)
 
     println("\nBW-regulated masters:")
     for (i <- masterNames.indices)
