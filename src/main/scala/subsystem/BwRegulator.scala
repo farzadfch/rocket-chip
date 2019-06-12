c8f436c03d06337ac44cd4675883b9e69de97c15
Farzad Farshchi <farzadfr@gmail.com>
2019-06-12 17:50:52 -0500

    bw-reg has independent counters for read and write. Removed the cost mechanism.

--- src/main/scala/subsystem/BwRegulator.scala
+++ src/main/scala/subsystem/BwRegulator.scala
@@ -26,24 +26,24 @@ class BwRegulator(
     val n = node.in.length
     require(n == node.out.length)
     require(n <= 32)
-    val memBase = p(ExtMem).get.base.U
 
     val w = 32
     val nDomains = n
     var masterNames = new Array[String](n)
     val enableBW = RegInit(false.B)
-    val wrCost = Reg(UInt(2.W))
-    val rdCost = Reg(UInt(2.W))
     val windowCntr = Reg(UInt(w.W))
     val windowSize = Reg(UInt(w.W))
-    val xactionCntrs = Reg(Vec(nDomains, UInt(w.W)))
-    val maxXactionRegs = Reg(Vec(nDomains, UInt(w.W)))
+    val readCntrs = Reg(Vec(nDomains, UInt(w.W)))
+    val maxReadRegs = Reg(Vec(nDomains, UInt(w.W)))
+    val writeCntrs = Reg(Vec(nDomains, UInt(w.W)))
+    val maxWriteRegs = Reg(Vec(nDomains, UInt(w.W)))
     val enableMasters = Reg(Vec(n, Bool()))
     val domainId = Reg(Vec(n, UInt(log2Ceil(nDomains).W)))
 
     when (windowCntr >= windowSize || !enableBW) {
       windowCntr := 0.U
-      xactionCntrs.foreach(_ := 0.U)
+      readCntrs.foreach(_ := 0.U)
+      writeCntrs.foreach(_ := 0.U)
     } .otherwise {
       windowCntr := windowCntr + 1.U
     }
@@ -53,44 +53,52 @@ class BwRegulator(
       val (in, edge_in) = node.in(i)
 
       val aIsAcquire = in.a.bits.opcode === TLMessages.AcquireBlock
-      val cost = Mux(in.a.bits.param === TLPermissions.NtoB, rdCost, wrCost)
+      val isRead = in.a.bits.param === TLPermissions.NtoB
+      val isWrite = in.a.bits.param === TLPermissions.NtoT || in.a.bits.param === TLPermissions.BtoT
 
       out <> in
       when (enableBW && enableMasters(i)) {
-        when (xactionCntrs(domainId(i)) + cost > maxXactionRegs(domainId(i))) {
+        when (readCntrs(domainId(i)) >= maxReadRegs(domainId(i)) ||
+          writeCntrs(domainId(i)) >= maxWriteRegs(domainId(i))) {
             out.a.valid := false.B
             in.a.ready := false.B
         }
         when (out.a.fire() && aIsAcquire) {
-          xactionCntrs(domainId(i)) := xactionCntrs(domainId(i)) + cost
+          when (isRead) {
+            readCntrs(domainId(i)) := readCntrs(domainId(i)) + 1.U
+          }
+          when (isWrite) {
+            writeCntrs(domainId(i)) := writeCntrs(domainId(i)) + 1.U
+          }
         }
       }
 
       masterNames(i) = edge_in.client.clients(0).name
     }
 
-    val enableBWCostField = Seq(0 -> Seq(RegField(enableBW.getWidth, enableBW,
-      RegFieldDesc("enableBW", "Enable BW-regulator")),
-      RegField(rdCost.getWidth, rdCost,
-        RegFieldDesc("rdCost", "Read cost")),
-      RegField(wrCost.getWidth, wrCost,
-        RegFieldDesc("wrCost", "Write cost"))))
+    val enableBWField = Seq(0 -> Seq(RegField(enableBW.getWidth, enableBW,
+      RegFieldDesc("enableBW", "Enable BW-regulator"))))
 
     val windowRegField = Seq(4*1 -> Seq(RegField(windowSize.getWidth, windowSize,
       RegFieldDesc("windowsize", "Size of the window"))))
 
-    val maxRegFields = maxXactionRegs.zipWithIndex.map { case (reg, i) =>
+    val maxReadRegFields = maxReadRegs.zipWithIndex.map { case (reg, i) =>
       4*(2 + i) -> Seq(RegField(reg.getWidth, reg,
-      RegFieldDesc(s"maxxaction$i", s"Maximum number of transactions for domain $i"))) }
+      RegFieldDesc(s"maxRead$i", s"Maximum number of reads for domain $i"))) }
 
-    val enableMastersField = Seq(4*(2+nDomains) -> enableMasters.zipWithIndex.map { case (bit, i) =>
+    val maxWriteRegFields = maxWriteRegs.zipWithIndex.map { case (reg, i) =>
+      4*(2+nDomains + i) -> Seq(RegField(reg.getWidth, reg,
+        RegFieldDesc(s"maxWrite$i", s"Maximum number of writes for domain $i"))) }
+
+    val enableMastersField = Seq(4*(2+2*nDomains) -> enableMasters.zipWithIndex.map { case (bit, i) =>
       RegField(bit.getWidth, bit, RegFieldDesc("enableMasters", s"Enable BW-regulator for ${masterNames(i)}")) })
 
     val domainIdFields = domainId.zipWithIndex.map { case (reg, i) =>
-      4*(3+nDomains + i) -> Seq(RegField(reg.getWidth, reg,
+      4*(3+2*nDomains + i) -> Seq(RegField(reg.getWidth, reg,
       RegFieldDesc(s"domainId$i", s"Domain ID for ${masterNames(i)}"))) }
 
-    regnode.regmap(enableBWCostField ++ windowRegField ++ maxRegFields ++ enableMastersField ++ domainIdFields: _*)
+    regnode.regmap(enableBWField ++ windowRegField ++ maxReadRegFields ++ maxWriteRegFields ++ enableMastersField ++
+      domainIdFields: _*)
 
     println("\nBW-regulated masters:")
     for (i <- masterNames.indices)
